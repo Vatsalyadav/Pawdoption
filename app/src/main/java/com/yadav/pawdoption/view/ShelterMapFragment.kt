8e8b33150cefc2e1d8f62ad063fc494a4308112c
Library User <lib-user@t9598.wpa.dal.ca>
2022-11-21 18:52:05 -0400

    shelter map and donate to map added

---
+++ app/src/main/java/com/yadav/pawdoption/view/ShelterMapFragment.kt
@@ -0,0 +1,12 @@
+package com.yadav.pawdoption.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yadav.pawdoption.R
+
+class ShelterMapFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shelter_map_fragment)
+    }
+}
