35ef108363f48e4486b778bcc12806222a0b6e22
kishan <ks317715@dal.ca>
2022-11-22 21:06:24 -0400

    map added

--- app/src/main/java/com/yadav/pawdoption/model/Shelter.kt
+++ app/src/main/java/com/yadav/pawdoption/model/Shelter.kt
@@ -5,10 +5,10 @@ import com.google.gson.annotations.SerializedName
 
 data class Shelter (
 
-    @SerializedName("id"          ) var id          : String? = null,
-    @SerializedName("name"        ) var name        : String? = null,
-    @SerializedName("description" ) var description : String? = null,
-    @SerializedName("address"     ) var address     : String? = null,
+    @SerializedName("id") var id          : String? = null,
+    @SerializedName("name") var name        : String? = null,
+    @SerializedName("description") var description : String? = null,
+    @SerializedName("address") var address     : String? = null,
     @SerializedName("latitude"    ) var latitude    : Double? = null,
     @SerializedName("longitude"   ) var longitude   : Double? = null,
     @SerializedName("pets"        ) var pets        : ArrayList<ShelterPet> = arrayListOf(),
