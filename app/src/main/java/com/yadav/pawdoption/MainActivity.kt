ebcd7c644d8c160206a0224f0fa3697dde057220
Vatsal Yadav <vatsal.yadav@dal.ca>
2022-11-23 09:18:14 -0400

    Added Bottom navigation bar

--- app/src/main/java/com/yadav/pawdoption/MainActivity.kt
+++ app/src/main/java/com/yadav/pawdoption/MainActivity.kt
@@ -3,7 +3,9 @@ package com.yadav.pawdoption
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.ui.AppBarConfiguration
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.yadav.pawdoption.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +15,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setBottomNavigation()
 
     }
+
+    // https://guides.codepath.com/android/Bottom-Navigation-Views
+    private fun setBottomNavigation() {
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_nav)
+        bottomNavigationView.selectedItemId = R.id.pets
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+
+                else -> true
+            }
+
+        }
+    }
+
 
 }
