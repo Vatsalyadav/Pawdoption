4da9950ba25e8f5c609c67a42be0ef0c1c802875
Khushalkumar Maganbhai Gondaliya <khush@t96f4.wpa.dal.ca>
2022-11-24 12:00:41 -0400

    working on data integration with pet details

--- app/src/main/java/com/yadav/pawdoption/model/User.kt
+++ app/src/main/java/com/yadav/pawdoption/model/User.kt
@@ -1,8 +1,11 @@
 package com.yadav.pawdoption.model
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.IgnoredOnParcel
+import kotlinx.android.parcel.Parcelize
 
-
+@Parcelize
 data class User(
 
     @SerializedName("id")
@@ -20,16 +23,22 @@ data class User(
     @SerializedName("image")
     var image: String? = null,
 
+
+
+) : Parcelable {
+    @IgnoredOnParcel
     @SerializedName("pets")
-    var pets: ArrayList<UserPet>? = null,
+    var pets: ArrayList<UserPet>? = null
 
+    @IgnoredOnParcel
     @SerializedName("appointment")
-    var appointments: ArrayList<Appointment>? = null,
+    var appointments: ArrayList<Appointment>? = null
 
+    @IgnoredOnParcel
     @SerializedName("lovedPets")
-    var lovedPets: ArrayList<UserLovedPet>? = null,
+    var lovedPets: ArrayList<UserLovedPet>? = null
 
+    @IgnoredOnParcel
     @SerializedName("donations")
     var donations: ArrayList<UserDonation>? = null
-
-    )
+}
